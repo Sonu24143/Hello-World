c08c8f19ba3ccfa68892d506178f2283421ae17e
Sonu24143 <sonu24143parekaden@gmail.com>
2016-12-29 16:15:35 +0530

    updated sample.py file[structured]

--- sample.py
+++ sample.py
@@ -1,5 +1,14 @@
 #!/usr/bin/python
-
-print("Hello World")
-exit(0)
+def sum(a,b):
+   return a+b
+def sub(a,b):
+  pass
+  
+def Main():
+  print("Hello World")
+  sum(10,20)
+  exit(0)
+  
+if __name__ == '__main__':
+  Main()
 
